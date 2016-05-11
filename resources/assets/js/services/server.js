ecbade1e289f462f9bfa028d42fec30f47bb0048
Dave Stewart <dave@davestewart.co.uk>
2016-05-11 03:21:48 +0100

    Added ping-pong request tracking

--- resources/assets/js/services/server.js
+++ resources/assets/js/services/server.js
@@ -7,29 +7,67 @@ function Server()
 Server.prototype =
 {
 
-	base:'',
+		base:'',
 
-	call:function(route, onSuccess, onFail)
-	{
-		var url = this.getCallUrl(location.origin + route);
-		return $.get(url, onSuccess).fail(onFail);
-	},
-	
-	open:function(url)
-	{
-		window.open(server.getCallUrl(url));
-	},
+		requestId:0,
 
-	getCallUrl:function(url)
-	{
-		return url.replace(/\/$/, '') + '?call=1';
-	},
+		requestCount:0,
 
-	load:function(route, onSuccess)
-	{
-		var url = this.base + route;
-		console.log(url);
-		return $.get(url, onSuccess);
-	}
+
+	// ------------------------------------------------------------------------------------------------
+	// methods
+
+		/**
+		 * Calls a sketchpad route and returns the result
+		 *
+		 * @param route			The full route, including the '/sketchpad/' portion
+		 * @param onSuccess
+		 * @param onFail
+		 * @returns {*}
+		 */
+		call:function(route, onSuccess, onFail)
+		{
+			var url = location.origin + this.getCallUrl(route);
+			return $
+				.get(url, function(data, status, xhr){
+					var requestId = xhr.getResponseHeader('X-Request-ID');
+					if(requestId == null || requestId == this.requestId)
+					{
+						onSuccess(data, status, xhr);
+					}
+				}.bind(this))
+				.fail(onFail);
+		},
+
+		/**
+		 * Opens a sketchpad route in a new window
+		 *
+		 * @param url
+		 */
+		open:function(route)
+		{
+			window.open(this.getCallUrl(route));
+		},
+
+		/**
+		 * Requests information from the server
+		 *
+		 * @param route			The partial route, from '/sketchpad/' onwards
+		 * @param onSuccess
+		 * @returns {*}
+		 */
+		load:function(route, onSuccess)
+		{
+			var url = this.base + route;
+			return $.get(url, onSuccess);
+		},
+
+	// ------------------------------------------------------------------------------------------------
+	// utilities
+
+		getCallUrl:function(url)
+		{
+			return url.replace(/\/$/, '') + '?call=1&requestId=' + (++this.requestId);
+		}
 
 };
