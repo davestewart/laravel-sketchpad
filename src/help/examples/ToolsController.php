c8b1e7685da31a489cd3eaac86133d49de0f65f5
Dave Stewart <dave@davestewart.co.uk>
2017-02-26 16:28:19 +0000

    Reordered example ToolsContoller methods

--- src/help/examples/ToolsController.php
+++ src/help/examples/ToolsController.php
@@ -16,85 +16,14 @@ class ToolsController extends Controller
 
 	/**
 	 * View the app state
+	 *
+	 * @group Application
 	 */
 	public function dumpApp()
 	{
 		dump(app());
 	}
 	
-	/**
-	 * See what's in the session
-	 *
-	 */
-	public function viewSession()
-	{
-		ls(\Session::all(), true);
-	}
-
-    /**
-     * Output the result of `phpinfo()`
-     *
-     * Note the use of escaping into HTML to output the style tag
-     * @param string $key
-     */
-	public function phpInfo($key = 'all')
-	{
-
-		$sections =
-		[
-			'general' => 1,
-			'credits' => 2,
-			'configuration' => 4,
-			'modules' => 8,
-			'environment' => 16,
-			'variables' => 32,
-			'license' => 64,
-			'all' => -1,
-		];
-		$section = isset($sections[$key])
-			? $sections[$key]
-			: -1;
-
-        $links = [];
-        foreach($sections as $key => $value)
-        {
-            $links[] = '<a href="?key=' .$key. '">' .$key. '</a> ';
-        }
-
-		?>
-		<style type="text/css">
-			#output pre {margin: 0; font-family: monospace;}
-			#output table a:hover {text-decoration: underline;}
-			#output table a:link {color: #009; text-decoration: none; background-color: #fff;}
-			#output table {border-collapse: collapse; border: 0; width: 100%; box-shadow: 1px 2px 3px #ccc;}
-			#output .center {text-align: center;}
-			#output .center th {text-align: center !important;}
-			#output td, th {border: 1px solid #666; font-size: 75%; vertical-align: baseline; padding: 4px 5px;}
-			#output h1 {font-size: 150%;}
-			#output h2 {font-size: 125%;}
-			#output .p {text-align: left;}
-			#output .e {background-color: #ccf; width: 300px; font-weight: bold;}
-			#output .h {background-color: #99c; font-weight: bold;}
-			#output .v {background-color: #ddd; max-width: 300px; overflow-x: auto;}
-			#output .v i {color: #999;}
-			#output img {float: right; border: 0;}
-			#output hr {background-color: #ccc; border: 0; height: 1px;}
-            #output .links{ padding: 10px; padding-top:0; border-bottom:1px solid #EEE; margin-bottom:10px; }
-		</style>
-		<?php
-
-		ob_start();
-		phpinfo($section);
-		$contents = ob_get_contents();
-		ob_end_clean();
-
-		$contents   = preg_replace('/^[\s\S]+?body>/', '', $contents);
-		$contents   = preg_replace('/<\/body>[\s\S]+$/', '', $contents);
-        echo '<div class="links">' . implode( ' | ', $links) . '</div>';
-		echo $contents;
-
-	}
-
 	/**
 	 * Show users in a table
 	 */
@@ -214,10 +143,84 @@ class ToolsController extends Controller
         return "Path '$_path' not found";
 	}
 
+	/**
+	 * See what's in the session
+	 * @group Environment
+	 */
+	public function viewSession()
+	{
+		ls(\Session::all(), true);
+	}
+
+    /**
+     * Output the result of `phpinfo()`
+     *
+     * Note the use of escaping into HTML to output the style tag
+     * @param string $key
+     */
+	public function phpInfo($key = 'all')
+	{
+
+		$sections =
+		[
+			'general' => 1,
+			'credits' => 2,
+			'configuration' => 4,
+			'modules' => 8,
+			'environment' => 16,
+			'variables' => 32,
+			'license' => 64,
+			'all' => -1,
+		];
+		$section = isset($sections[$key])
+			? $sections[$key]
+			: -1;
+
+        $links = [];
+        foreach($sections as $key => $value)
+        {
+            $links[] = '<a href="?key=' .$key. '">' .$key. '</a> ';
+        }
+
+		?>
+		<style type="text/css">
+			#output pre {margin: 0; font-family: monospace;}
+			#output table a:hover {text-decoration: underline;}
+			#output table a:link {color: #009; text-decoration: none; background-color: #fff;}
+			#output table {border-collapse: collapse; border: 0; width: 100%; box-shadow: 1px 2px 3px #ccc;}
+			#output .center {text-align: center;}
+			#output .center th {text-align: center !important;}
+			#output td, th {border: 1px solid #666; font-size: 75%; vertical-align: baseline; padding: 4px 5px;}
+			#output h1 {font-size: 150%;}
+			#output h2 {font-size: 125%;}
+			#output .p {text-align: left;}
+			#output .e {background-color: #ccf; width: 300px; font-weight: bold;}
+			#output .h {background-color: #99c; font-weight: bold;}
+			#output .v {background-color: #ddd; max-width: 300px; overflow-x: auto;}
+			#output .v i {color: #999;}
+			#output img {float: right; border: 0;}
+			#output hr {background-color: #ccc; border: 0; height: 1px;}
+            #output .links{ padding: 10px; padding-top:0; border-bottom:1px solid #EEE; margin-bottom:10px; }
+		</style>
+		<?php
+
+		ob_start();
+		phpinfo($section);
+		$contents = ob_get_contents();
+		ob_end_clean();
+
+		$contents   = preg_replace('/^[\s\S]+?body>/', '', $contents);
+		$contents   = preg_replace('/<\/body>[\s\S]+$/', '', $contents);
+        echo '<div class="links">' . implode( ' | ', $links) . '</div>';
+		echo $contents;
+
+	}
+
+
     /**
      * Just for fun!
      *
-     * @return string
+     * @group Other
      */
 	public function randomCat()
     {
