9cf2600b4da3dd16a04baed217b2192e8075dbb7
Dave Stewart <dave@davestewart.co.uk>
2017-03-21 22:33:52 +0000

    Updated utils

---
+++ resources/assets/js/functions/utils.js
@@ -0,0 +1,24 @@
+export function clone(obj)
+{
+	return JSON.parse(JSON.stringify(obj));
+}
+
+export function dump()
+{
+	Array.prototype.slice.call(arguments).map(obj => console.log(clone(obj)))
+}
+
+let context;
+
+export function getTextWidth(text, font)
+{
+	// re-use canvas object for better performance
+	if (!context) 
+	{
+		const canvas = document.createElement('canvas');
+		context = canvas.getContext('2d');
+	}
+	context.font = font;
+	const metrics = context.measureText(text);
+	return metrics.width;
+}
