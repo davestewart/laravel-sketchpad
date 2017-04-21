2b93c2758abb31051c6c0ec94b34329e56143af8
Dave Stewart <dave@davestewart.co.uk>
2017-04-21 14:25:35 +0100

    Cleaned up example controller method

--- package/setup/views/example.blade.php
+++ package/setup/views/example.blade.php
@@ -1,7 +1,7 @@
 <h3>Hello {{ $name }}!</h3>
 <p>This is an example controller and view, just to get you started:</p>
 <ul>
-	<li>the controller is located at <code>{{ $config->controllers['project'] }}</code></li>
-	<li>the view is loaded from <code>{{ $config->views }}</code> using <code>view('sketchpad::example')</code></li>
+	<li>the controller is located at <code>{{ $controllers }}</code></li>
+	<li>the view is loaded from <code>{{ $views }}</code> using <code>view('sketchpad::example')</code></li>
 </ul>
 <p>You can edit, rename, replace or delete this controller and/or methods as you see fit.</p>
